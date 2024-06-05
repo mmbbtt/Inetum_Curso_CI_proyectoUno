052f4569d4aff516c159ac401854ff946e8f696a
Marcelino Barreiro <36330303+mmbbtt@users.noreply.github.com>
2024-06-05 15:07:03 +0200

    Added files generated by Spring

---
+++ src/main/java/mbarreiro/cursos/ic/proyectoUno/ProyectoUnoApplication.java
@@ -0,0 +1,13 @@
+package mbarreiro.cursos.ic.proyectoUno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoUnoApplication.class, args);
+	}
+
+}
